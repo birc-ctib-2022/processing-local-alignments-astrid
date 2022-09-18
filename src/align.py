d495cb1bf2f0cd4e9402d5dc6077ad5dea11034a
AstridDahl <88832493+AstridDahl@users.noreply.github.com>
2022-09-18 20:49:56 +0200

    alignvers1

--- src/align.py
+++ src/align.py
@@ -1,6 +1,10 @@
 """A module for translating between alignments and edits sequences."""
 
 
+from operator import le
+from re import I
+
+
 def align(x: str, y: str, edits: str) -> tuple[str, str]:
     """Align two sequences from a sequence of edits.
 
@@ -16,7 +20,30 @@ def align(x: str, y: str, edits: str) -> tuple[str, str]:
     ('ACCACAGT-CATA', 'A-CAGAGTACAAA')
 
     """
-    return "", ""
+    x_align = ''
+    y_align = ''
+    i = 0
+    j = 0
+
+    for _ in edits:
+
+        if _ == 'M':
+            x_align += x[i]
+            y_align += y[j]
+            i += 1
+            j += 1
+
+        elif _ == 'I':
+            x_align += '-'
+            y_align += y[j]
+            j += 1
+            
+        elif _ == 'D':
+            y_align += '-'
+            x_align += x[i]
+            i += 1
+
+    return x_align, y_align
 
 
 def edits(x: str, y: str) -> str:
@@ -33,4 +60,21 @@ def edits(x: str, y: str) -> str:
     'MDMMMMMMIMMMM'
 
     """
-    return ""
+    edits = ''
+    
+    for _ in range(len(x)):
+        print(_)
+
+        if x[_] in 'ATGC' and y[_] in 'ATGC':
+            edits += 'M'
+
+        elif x[_] == '-':
+            edits += 'I'
+
+        elif y[_] == '-':
+            edits += 'D'
+
+    return edits
+
+print(align("ACCACAGTCATA", "ACAGAGTACAAA", "MDMMMMMMIMMMM"))
+print(edits('ACCACAGT-CATA', 'A-CAGAGTACAAA'))
